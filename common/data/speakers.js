80f28379b270f369a1759aff486f64496231eee9
mdedeu <mdedeu@itba.edu.ar>
2022-01-25 19:01:22 -0300

    add Jury Data

--- common/data/speakers.js
+++ common/data/speakers.js
@@ -1,23 +1,23 @@
 export const speakers = [
   {
-    name: "Matias Wolo",
-    title: "CEO de algo",
+    name: "Matías Woloski",
+    title: "CTO & Co-founder de Auth0",
     img: "https://www.endeavor.org.ar/wp-content/uploads/2019/02/Auth0-e1621389968909.jpg",
     description:
-      "DescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcionDescripcion",
+      "Matías es Ingeniero en informática de la Universidad de Buenos Aires. Trabajó en Southworks, uno de los proveedores más confiables de Microsoft, durante 8 años donde ayudó a varios grupos de Microsoft como patterns & practices, DPE, Architecture Strategy y otros. Su trabajo con Microsoft patterns & practices lo llevo a cruzarse con Eugenio Pace, su socio actual, con el que fundó Auth0 en 2013.",
   },
   {
-    name: "Matias otro",
-    title: "CEO de otra cosa",
-    img: "https://www.endeavor.org.ar/wp-content/uploads/2019/02/Auth0-e1621389968909.jpg",
+    name: "Mariano Mayer",
+    title: "Co-Founder de Newtopia VC",
+    img: "https://www.produccion.gob.ar/wp-content/uploads/2017/07/Mariano-Mayer-1.jpg",
     description:
-      "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd",
+      " Mariano es co-Founder y socio principal de Newtopia, una inversora ángel que tiene como objetivo convertirse en uno de los principales motores dentro del ecosistema emprendedor latinoamericano. También implementó  políticas publicas en el gobierno de la ciudad y la nación para apoyar a emprendedores, venture capitals, aceleradoras e incubadoras, por más de 100MM USD.",
   },
   {
-    name: "Tercero",
-    title: "CEO de otra cosa",
-    img: "https://www.endeavor.org.ar/wp-content/uploads/2019/02/Auth0-e1621389968909.jpg",
+    name: "Gabriel Gruber",
+    title: "Co-founder & CEO de Exactly Finance",
+    img: "https://miro.medium.com/fit/c/262/262/1*Sm7jkT5APmJC0bu4t_ZS6w.jpeg",
     description:
-      "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd",
+      "Gabriel es fundador y ex-CEO de Properati, una startup inmobiliaria con presencia en Argentina y en la región que le vendió años atrás a OLX. Es licenciado en Economía de la UBA, trabajó en el Banco Central de Argentina (BCRA) y en Tilton Capital. Actualmente está emprendiendo en web3 con Exactly Finance, un protocolo de inversión que permite obtener renta fija en DeFi",
   },
 ];
