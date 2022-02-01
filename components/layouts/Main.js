1280486e64ff24212ec47d0d70d6b13099ba8d8c
mdedeu <mdedeu@itba.edu.ar>
2022-02-01 18:00:44 -0300

    meta indentation

--- components/layouts/Main.js
+++ components/layouts/Main.js
@@ -12,19 +12,18 @@ const Main = ({ children, router }) => {
         <meta name="author" content="Juan Pablo Oriana" />
           <title>HackIT-BA! by Computer Society ITBA</title>
           <meta name="title" content="HackIT-BA! by Computer Society ITBA"/>
-              <meta name="description" content="HackIT-BA es una hackathon gratis de 36hs el 1, 2 y 3 de abril, enmarcada en 3 categorías principales: Ciberseguridad y privacidad, Productividad y Automatización e Inclusión Financera."/>
-                  <meta property="og:type" content="website"/>
-                      <meta property="og:url" content="https://metatags.io/"/>
-                          <meta property="og:title" content="HackIT-BA! by Computer Society ITBA"/>
-                              <meta property="og:description" content="HackIT-BA es una hackathon gratis de 36hs el 1, 2 y 3 de abril, enmarcada en 3 categorías principales: Ciberseguridad y privacidad, Productividad y Automatización e Inclusión Financera."/>
-                                  <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+          <meta name="description" content="HackIT-BA es una hackathon gratis de 36hs el 1, 2 y 3 de abril, enmarcada en 3 categorías principales: Ciberseguridad y privacidad, Productividad y Automatización e Inclusión Financera."/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://metatags.io/"/>
+          <meta property="og:title" content="HackIT-BA! by Computer Society ITBA"/>
+          <meta property="og:description" content="HackIT-BA es una hackathon gratis de 36hs el 1, 2 y 3 de abril, enmarcada en 3 categorías principales: Ciberseguridad y privacidad, Productividad y Automatización e Inclusión Financera."/>
+          <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
 
-
-                                      <meta property="twitter:card" content="summary_large_image"/>
-                                          <meta property="twitter:url" content="https://metatags.io/"/>
-                                              <meta property="twitter:title" content="HackIT-BA! by Computer Society ITBA"/>
-                                                  <meta property="twitter:description" content="HackIT-BA es una hackathon gratis de 36hs el 1, 2 y 3 de abril, enmarcada en 3 categorías principales: Ciberseguridad y privacidad, Productividad y Automatización e Inclusión Financera."/>
-                                                      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content="https://metatags.io/"/>
+          <meta property="twitter:title" content="HackIT-BA! by Computer Society ITBA"/>
+          <meta property="twitter:description" content="HackIT-BA es una hackathon gratis de 36hs el 1, 2 y 3 de abril, enmarcada en 3 categorías principales: Ciberseguridad y privacidad, Productividad y Automatización e Inclusión Financera."/>
+          <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
         </Head>
       <Navbar />
       <Container maxW="container.lg" pt={14}>
