457233c477a184887b21ae52ad70fad4dbd0b0b6
srosati <srosati@itba.edu.ar>
2022-12-20 12:33:42 -0300

    feat: upgraded to react 18

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ const Website = ({ Component, pageProps, router }) => {
     <ChakraProvider theme={theme}>
       <Fonts />
       <Layout router={router}>
-        <AnimatePresence exitBeforeEnter initial={true}>
+        <AnimatePresence mode='wait' initial={true}>
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Layout>
