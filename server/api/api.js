592e73b6875baf554b884781d4717d1beaa6f7a3
JoseMenta <jrmenta2@gmail.com>
2022-12-24 00:47:22 -0300

    First version of authentication middleware

--- server/api/api.js
+++ server/api/api.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const user = require('./user/user')
+const authMiddleware = require('./middleware/authMiddleware')
 // /api endpoints
-router.use('/user',user)
+router.use('/user',authMiddleware,user) //el middleware aca es para testear
 
 router.get('/hello',(req,res)=>{
     res.status(200).send({message:'Hello Api!'})
--- server/api/middleware/authMiddleware.js
+++ server/api/middleware/authMiddleware.js
@@ -1,5 +1,58 @@
-// function middleware (req, res, next) {
-//     console.log('express middleware')
-//     next()
-// }
-// module.exports = {middleware}
+const path = require('path');
+const admin = require("firebase-admin");
+//por favor, si alguien encuentra una manera mas linda de hacer esto cambielo
+//no queria hacer path relativo 
+const serviceAccount = require(path.join(process.cwd(),'hackitba-admin-config.json'));
+const app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const auth = app.auth()
+
+//Usar si se quire crear un token para testear
+// const {initializeApp} = require('firebase/app')
+// const { getAuth , signInWithEmailAndPassword} = require ("firebase/auth");
+// const firebaseConfig = {
+//     //completar con los datos que se obtienen en la configuracion del proyecto
+//   };
+  
+//
+// initializeApp(firebaseConfig);
+
+function error(code,message){
+    return {error:{
+        code:code,
+        message:message
+    }}
+}
+
+function authMiddleware (req, res, next) {
+    const authHeader = req.headers['authorization'] //no tengo idea por que si los pongo con A mayuscula no lo reconoce
+    if(!authHeader){
+        res.status(401).send(error(1,"Misising Authorization header"))
+        return
+    }
+    const strings = authHeader.split(' ')
+    if(!strings[1] ||  strings[0]!=='Bearer'){
+        res.status(401).send(error(2,'Bad access token'))
+        return
+    }
+    //Usar si se quire crear un token para testear
+    // signInWithEmailAndPassword(getAuth(),"jmentasti@itba.edu.ar","TestUser22").then(()=>{
+    //     getAuth().currentUser.getIdToken().then((token)=>{
+    //         console.log(token)
+    //     })
+    // })
+
+    //Debe ser un IdToken, no otro
+    auth.verifyIdToken(strings[1])
+        .then((decodedToken)=>{
+            const uid = decodedToken.uid
+            console.log(uid)
+            next()
+        })
+        .catch((err)=>{
+            console.log(err)
+            res.status(401).send(error(3,"Invalid token"))
+        })
+}
+module.exports = authMiddleware
