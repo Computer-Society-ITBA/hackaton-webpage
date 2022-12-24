8a961b11c07852ebce63371f99ebfa8c737d1f90
JoseMenta <jrmenta2@gmail.com>
2022-12-24 17:41:10 -0300

    Using .env file for credentials

--- server/api/middleware/authMiddleware.js
+++ server/api/middleware/authMiddleware.js
@@ -1,21 +1,36 @@
+require('dotenv').config()
 const path = require('path');
 const admin = require("firebase-admin");
 //por favor, si alguien encuentra una manera mas linda de hacer esto cambielo
 //no queria hacer path relativo 
-const serviceAccount = require(path.join(process.cwd(),'hackitba-admin-config.json'));
+const serviceAccount = {
+    type:                           process.env.ADMIN_TYPE,
+    project_id:                     process.env.ADMIN_PROJECT_ID,
+    private_key_id:                 process.env.ADMIN_PRIVATE_KEY_ID,
+    private_key:                    process.env.ADMIN_PRIVATE_KEY,
+    client_email:                   process.env.ADMIN_CLIENT_EMAIL,
+    client_id:                      process.env.ADMIN_CLIENT_ID,
+    auth_uri:                       process.env.ADMIN_AUTH_URI,
+    token_uri:                      process.env.ADMIN_TOKEN_URI,
+    auth_provider_x509_cert_url:    process.env.ADMIN_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url:           process.env.ADMIN_CLIENT_X509_CERT_URL
+}
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const auth = app.auth()
-
 //Usar si se quire crear un token para testear
 // const {initializeApp} = require('firebase/app')
 // const { getAuth , signInWithEmailAndPassword} = require ("firebase/auth");
 // const firebaseConfig = {
-//     //completar con los datos que se obtienen en la configuracion del proyecto
-//   };
-  
-//
+//     apiKey:             process.env.CLIENT_API_KEY,
+//     authDomain:         process.env.CLIENT_AUTH_DOMAIN,
+//     projectId:          process.env.CLIENT_PROJECT_ID,
+//     storageBucket:      process.env.CLIENT_STORAGE_BUCKET,
+//     messagingSenderId:  process.env.CLIENT_MESSAGING_SENDER_ID,
+//     appId:              process.env.CLIENT_APP_ID,
+//     measurementId:      process.env.CLIENT_MEASUREMENT_ID
+// };
 // initializeApp(firebaseConfig);
 
 function error(code,message){
