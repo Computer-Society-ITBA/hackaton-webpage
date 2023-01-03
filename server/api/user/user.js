f5e7478928a38ad876301c7858f5709155ef6f20
JoseMenta <jrmenta2@gmail.com>
2023-01-02 21:44:27 -0300

    First implementation of endpoint. Tested with test user

--- server/api/user/user.js
+++ server/api/user/user.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router()
 const {roleMiddleware, ROLE_ADMIN, ROLE_MENTOR, ROLE_JURY} = require('../middleware/roleMiddleware')
 const {getUser, changePassword} = require('./auth_util')
+const authMiddleware = require('../middleware/authMiddleware')
 const {error} = require('../util')
 // Todos los endpoints necesitan autenticacion (se require en el nivel de api.js)
 // /user endpoints
 router.get('/hello',(req,res)=>{
     res.status(200).send({message:'Hello User!'})
 })
-router.get('/:userId',async (req, res)=>{
+router.get('/:userId',authMiddleware,async (req, res)=>{
     const uid = req.params.userId
     if(uid===res.locals.userInfo.uid){
         //busca la informacion de su usuario
@@ -29,10 +30,25 @@ router.get('/:userId',async (req, res)=>{
         }
         res.status(200).send(ans)
     })
-    return 
 })
-router.put('/:userId/password',async(req,res)=>{
-
+//No se si es necesario agregar el middleware de rol o directamente lo dejamos para todos
+router.put('/:userId/password',authMiddleware,async(req,res)=>{
+    const uid = req.params.userId
+    const password = req.body.password
+    if(!uid || !password){
+        res.status(202).send(error(1,"Missing password or userId"))
+        return
+    }
+    if(uid!==res.locals.userInfo.uid){
+        res.status(401).send(error(2,"Password can be change only by the account owner"))
+        return
+    }
+    const ans = await changePassword(uid,password)
+    if(ans.error){
+        res.status(404).send(ans)
+        return
+    }
+    res.status(200).send(ans)
 })
 
 module.exports = router
