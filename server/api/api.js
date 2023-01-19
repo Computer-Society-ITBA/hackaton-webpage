827bffc78b5d06e79442d627b3e00639f002656a
JoseMenta <jrmenta2@gmail.com>
2023-01-18 21:45:27 -0300

    Changes to return res...

--- server/api/api.js
+++ server/api/api.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const user = require('./user/user')
 const test_config = require('./test/test_config')
 const test_role_middleware = require('./test/test_role')
-const authMiddleware = require('./middleware/authMiddleware')
+
 
 // /api endpoints
-router.use('/users',authMiddleware,user) //el middleware aca es para testear
+router.use('/users',user) //el middleware aca es para testear
 
 //TODO: Delete
 router.use('/test/config',test_config)
--- server/api/user/user.js
+++ server/api/user/user.js
@@ -4,12 +4,13 @@ const {roleMiddleware, ROLE_ADMIN, ROLE_MENTOR, ROLE_JURY, ROLE_USER} = require(
 const {getUser, changePassword} = require('./auth_util')
 const {error} = require('../util')
 const {addMember, editMember, deleteMember, getMembers, editQualification} = require('./firestore_util')
+const authMiddleware = require('../middleware/authMiddleware')
 // Todos los endpoints necesitan autenticacion (se require en el nivel de api.js)
 // /user endpoints
 router.get('/hello',(req,res)=>{
     res.status(200).send({message:'Hello User!'})
 })
-router.get('/:userId',async (req, res)=>{
+router.get('/:userId',authMiddleware,async (req, res)=>{
     const uid = req.params.userId
     if(uid===res.locals.userInfo.uid){
         //busca la informacion de su usuario
@@ -36,88 +37,74 @@ router.put('/:userId/password',async(req,res)=>{
 
 })
 //TODO: agregar middleware de auth cuando se haya mergeado con el de cambiar contraseña
-router.put('/:userId/qualified', roleMiddleware([ROLE_ADMIN]),async(req, res)=>{
+router.put('/:userId/qualified',authMiddleware, roleMiddleware([ROLE_ADMIN]),async(req, res)=>{
     const qualifiedValue = req.body.qualified
     if(qualifiedValue===undefined){ // no hacer !qualified porque si mandan false entra
-        res.status(400).send(error(1,"Missing qualified body field"))
-        return 
+        return res.status(400).send(error(1,"Missing qualified body field")) 
     }
     const ans = await editQualification(res.locals.userInfo.uid,qualifiedValue)
     if(ans.error){
-        res.status(400).send(ans)
-        return
+        return res.status(400).send(ans)
     }
     res.status(200).send(ans)
 })
 //Estas funciones son muy parecidas, despues veo como puedo juntarlo 
-router.post('/:userId/members',roleMiddleware([ROLE_USER]), async (req, res)=> {
+router.post('/:userId/members',authMiddleware,roleMiddleware([ROLE_USER]), async (req, res)=> {
     const {full_name, dni, email} = req.body 
     const uid = req.params.userId
     if(uid!==res.locals.userInfo.uid){
-        res.status(401).send(error(2,"Members can be created only by the team account"))
-        return
+        return res.status(401).send(error(2,"Members can be created only by the team account"))
     }
     if(!full_name || !dni || !email){
-        res.status(400).json(error(1, "Missing member information"))
-        console.log()
-        return 
+        return res.status(400).json(error(1, "Missing member information"))
     }
     const ans = await addMember(res.locals.userInfo.uid, dni, email, full_name)
     if(ans.error){
-        res.status(400).send(ans)
-        return
+        return res.status(400).send(ans)
     }
     res.status(200).send(ans)
 })
-router.patch('/:userId/members/:memberId',roleMiddleware([ROLE_USER]), async (req, res)=> {
+router.patch('/:userId/members/:memberId',authMiddleware,roleMiddleware([ROLE_USER]), async (req, res)=> {
     const {full_name, dni, email} = req.body 
     const uid = req.params.userId
     const memberId = req.params.memberId
     if(uid!==res.locals.userInfo.uid){
-        res.status(401).send(error(2,"Members can be changed only by the team account"))
-        return
+        return res.status(401).send(error(2,"Members can be changed only by the team account"))
     }
     if(!full_name && !dni && !email){//damos error solo si no pasa info de algun campo
-        res.status(400).send(error(1, "Missing member information"))
-        return 
+        return res.status(400).send(error(1, "Missing member information")) 
     }
     const ans = await editMember(uid, memberId, dni, email, full_name)
     if(ans.error){
-        res.status(400).send(ans)
-        return
+        return res.status(400).send(ans)
     }
     res.status(200).send(ans)
 })
-router.delete('/:userId/members/:memberId', roleMiddleware([ROLE_USER]), async(req, res)=>{
+router.delete('/:userId/members/:memberId',authMiddleware, roleMiddleware([ROLE_USER]), async(req, res)=>{
     const uid = req.params.userId
     const memberId = req.params.memberId
     if(uid!==res.locals.userInfo.uid){
-        res.status(401).send(error(2,"Members can be changed only by the team account"))
-        return
+        return res.status(401).send(error(2,"Members can be changed only by the team account"))
     }
     const ans = await deleteMember(uid, memberId)
     if(ans.error){
-        res.status(400).send(ans)
-        return
+        return res.status(400).send(ans)
     }
     res.status(200).send(ans)
 })
-router.get('/:userId/members',async(req, res)=>{
+router.get('/:userId/members',authMiddleware,async(req, res)=>{
     const uid = req.params.userId
     if(uid===res.locals.userInfo.uid){
         const ans = await getMembers(uid)
         if(ans.error){
-            res.status(400).send(ans)
-            return
+            return res.status(400).send(ans)
         }
-        res.status(200).send(ans)
-        return
+        return res.status(200).send(ans)
     }
     roleMiddleware([ROLE_ADMIN, ROLE_JURY, ROLE_MENTOR])(req, res, async()=>{
         const ans = await getMembers(uid)
         if(ans.error){
-            res.status(400).send(ans)
-            return
+            return res.status(400).send(ans)
         }
         res.status(200).send(ans)
     })
