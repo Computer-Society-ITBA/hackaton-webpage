2dd9d82c6aa4f509d26c25516476d0117952d34c
JoseMenta <jrmenta2@gmail.com>
2023-01-18 21:34:35 -0300

    Adding selfMiddleware, changes to return with res... and to response code

--- server/api/user/user.js
+++ server/api/user/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const {roleMiddleware, ROLE_ADMIN, ROLE_MENTOR, ROLE_JURY} = require('../middleware/roleMiddleware')
 const {getUser, changePassword} = require('./auth_util')
 const authMiddleware = require('../middleware/authMiddleware')
+const selfMiddleware = require('../middleware/selfMiddleware')
 const {error} = require('../util')
 // Todos los endpoints necesitan autenticacion (se require en el nivel de api.js)
 // /user endpoints
@@ -15,38 +16,32 @@ router.get('/:userId',authMiddleware,async (req, res)=>{
         //busca la informacion de su usuario
         const ans = (await getUser(uid))
         if(ans.error){
-            res.status(404).send(ans)
-            return
+            return res.status(404).send(ans)
         }
-        res.status(200).send(ans)
-        return //hacer send no hace que se deje de ejecutar 
+        return res.status(200).send(ans)
     }
     //procesar request
     roleMiddleware([ROLE_ADMIN,ROLE_JURY,ROLE_MENTOR])(req,res,async ()=>{
         const ans = (await getUser(uid))
         if(ans.error){
-            res.status(404).send(ans)
-            return
+            return res.status(404).send(ans)
         }
         res.status(200).send(ans)
     })
 })
 //No se si es necesario agregar el middleware de rol o directamente lo dejamos para todos
-router.put('/:userId/password',authMiddleware,async(req,res)=>{
+router.put('/:userId/password',authMiddleware,selfMiddleware,async(req,res)=>{
     const uid = req.params.userId
     const password = req.body.password
-    if(!uid || !password){
-        res.status(202).send(error(1,"Missing password or userId"))
-        return
+    if(!password){
+        return res.status(400).send(error(1,"Missing password"))
     }
     if(uid!==res.locals.userInfo.uid){
-        res.status(403).send(error(2,"Password can be changed only by the account owner"))
-        return
+        return res.status(403).send(error(2,"Password can be changed only by the account owner"))
     }
     const ans = await changePassword(uid,password)
     if(ans.error){
-        res.status(400).send(ans)
-        return
+        return res.status(400).send(ans)
     }
     res.status(200).send(ans)
 })
