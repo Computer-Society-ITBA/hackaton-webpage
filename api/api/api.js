8d96c1fc6fdf41d5fc9790ec31be4537fe02af8c
srosati <srosati@itba.edu.ar>
2023-02-21 20:10:26 -0300

    fix: api

--- api/api/api.js
+++ api/api/api.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const user = require('./user/user')
-const test_config = require('./test/test_config')
-const test_role_middleware = require('./test/test_role')
+// const test_config = require('./test/test_config')
+// const test_role_middleware = require('./test/test_role')
 const mail = require('./mail/mail')
 const cors = require('cors')
 
@@ -13,11 +13,11 @@ router.use(cors({
 router.use('/users',user) //el middleware aca es para testear
 router.use('/mail',mail)
 //TODO: Delete
-router.use('/test/config', test_config)
-router.use('/test/role', test_role_middleware)
+// router.use('/test/config', test_config)
+// router.use('/test/role', test_role_middleware)
 
-router.get('/hello', (req, res) => {
-    res.status(200).send({ message: 'Hello Api!' })
-})
+// router.get('/hello', (req, res) => {
+//     res.status(200).send({ message: 'Hello Api!' })
+// })
 
 module.exports = router
