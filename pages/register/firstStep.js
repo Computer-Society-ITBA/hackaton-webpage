ee100abd0a7b9ce550c7f6e2b213bebcce0cdeda
JoseMenta <jrmenta2@gmail.com>
2023-02-20 21:19:09 -0300

    Change validation format on first step

--- pages/register/firstStep.js
+++ pages/register/firstStep.js
@@ -32,14 +32,13 @@ const theme = extendTheme({
 const FirstStep = ({setName, name, nextStep}) => {
 
   const [value, setValue] = useState(name);
-  const handleChange = (event) => {setValue(event.target.value);}
+  const [invalidValue, setInvalidValue] = useState(false)
+  const handleChange = (event) => {
+    setValue(event.target.value)
+    setInvalidValue(event.target.value==='')
+  }
 
   const moveForward = () =>{
-
-    if (value == ""){
-      return
-    }
-
     setName(value)
     nextStep()
   }
@@ -56,8 +55,8 @@ const FirstStep = ({setName, name, nextStep}) => {
        </Breadcrumb>
         <VStack paddingTop='5%'>
             <Text fontSize={['xl', '2xl', '3xl']} paddingBottom='5%'>Ingresa aca el <Text as="span" color="orange"> nombre de tu equipo:</Text></Text>
-            <Input value={value} onChange={handleChange} textColor={"black"} bg="white" focusBorderColor='white' />
-            <Text fontSize={['2xs', 'lg', 'xl']}>{value===""? "El nombre no puede estar vacio" : ""}</Text>
+            <Input borderWidth='1.5px' isInvalid={invalidValue} errorBorderColor="red.500" value={value} onChange={handleChange} textColor={"black"} bg="white" focusBorderColor='white' />
+            <Text fontSize={['2xs', 'lg', 'xl']} color='red'>{invalidValue? "El nombre no puede estar vacio" : ""}</Text>
             <Text fontSize={['xs', 'xl', '2xl']}><Text as="span" color="orange"> Recorda</Text>: el nombre debe ser apropiado para la competencia</Text>
         </VStack>
 
@@ -71,6 +70,7 @@ const FirstStep = ({setName, name, nextStep}) => {
             color="black"
             variant="solid"
             bgColor="orange"
+            isDisabled={invalidValue||value===""}
             > 
             Siguiente 
           </Button>
