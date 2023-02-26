913e7216493c110a6d2187d4c6df0fbf2ad9149b
srosati <srosati@itba.edu.ar>
2023-02-25 22:46:29 -0300

    fix: styling

--- components/SponsorLogo.js
+++ components/SponsorLogo.js
@@ -1,31 +1,26 @@
-import { Img, Box, Center } from "@chakra-ui/react";
-import Link from "next/dist/client/link";
+import { Img, Box, Center, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const SponsorLogo = ({ link, logo, name, width, height, style }) => {
   return (
-    <Box mx={8} {...style}>
-      <Link href={link} passHref>
-        <a target="_blank">
-          <Center>
-            <Img
-              src={logo}
-              alt={name}
-              width={width ? width : "auto"}
-              height={height ? height : "auto"}
-              cursor="pointer"
-              _hover={{
-                transform: "scale(1.1)",
-                transition: "ease-in-out 0.1s",
-              }}
-              _active={{
-                transform: "scale(0.9)",
-                transition: "ease-in-out 0.1s",
-              }}
-            />
-          </Center>
-        </a>
-      </Link>
-    </Box>
+    <Link {...style} href={link} isExternal height='100%' mx={8}>
+      <Img
+        src={logo}
+        alt={name}
+        width={width ? width : "auto"}
+        height={height ? height : "auto"}
+        cursor="pointer"
+        _hover={{
+          transform: "scale(1.1)",
+          transition: "ease-in-out 0.1s",
+        }}
+        _active={{
+          transform: "scale(0.9)",
+          transition: "ease-in-out 0.1s",
+        }}
+        objectFit="contain"
+      />
+    </Link>    
   );
 };
 
