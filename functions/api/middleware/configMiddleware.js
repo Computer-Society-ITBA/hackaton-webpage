45c2b0abc2de3eba88016efdb0eee02a869fa3b8
Mauro Sambartolomeo <msambartolomeo@itba.edu.ar>
2024-01-22 14:57:27 -0300

    add api config middleware

---
+++ functions/api/middleware/configMiddleware.js
@@ -0,0 +1,20 @@
+const { inscriptionsEnabled, submissionsEnabled } = require('../config/util')
+const { error } = require('../util')
+
+function inscriptionsOpenMiddleware(_, res, next) {
+    if (!inscriptionsEnabled()) {
+        return res.status(403).send(error(2, "Inscriptions are not open yet!"));
+    }
+
+    next();
+}
+
+function submissionsOpenMiddleware(_, res, next) {
+    if (!submissionsEnabled()) {
+        return res.status(403).send(error(2, "Submissions are not open yet!"));
+    }
+
+    next();
+}
+
+module.exports = { inscriptionsOpenMiddleware, submissionsOpenMiddleware }
